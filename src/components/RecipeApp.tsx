3b32c04f35a4486210706550afe7fdaadc0548d8
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-05-18 22:08:39 +0530

    Update RecipeApp.tsx

--- src/components/RecipeApp.tsx
+++ src/components/RecipeApp.tsx
@@ -89,7 +89,7 @@ const RecipeApp = () => {
             </>
           ) : (
               <>
-              <button onClick={handleAddRecipe} className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+              <button onClick={handleAddRecipe} className="bg-green-400 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
                   Add Recipe
               </button>
               </>
